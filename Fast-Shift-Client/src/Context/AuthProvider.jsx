5cced9c5ec79543f5fa1ef8608149b31fba84a18
Afridium <afridirafi2@gmail.com>
2026-07-18 13:51:21 +0600

    added signInWithGoogle

--- Fast-Shift-Client/src/Context/AuthProvider.jsx
+++ Fast-Shift-Client/src/Context/AuthProvider.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import { auth } from '../firebaseInit';
+import { GoogleAuthProvider } from "firebase/auth";
+const provider = new GoogleAuthProvider();
 
 const AuthProvider = ({children}) => {
 
@@ -12,10 +14,15 @@ const AuthProvider = ({children}) => {
     const loginUsingEmailAndPassword = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
+    
+    const signInWithGoogle = () => {
+        return signInWithPopup(auth, provider);
+    }
 
     const userInfo = {
         registerUsingEmailAndPassword,
         loginUsingEmailAndPassword,
+        signInWithGoogle
     }
     return (
         <AuthContext value={userInfo}>
--- Fast-Shift-Client/src/pages/AuthPages/Register.jsx
+++ Fast-Shift-Client/src/pages/AuthPages/Register.jsx
@@ -6,11 +6,17 @@ import { register } from 'swiper/element';
 import useAuth from '../../CustomHooks/useAuth';
 const Register = () => {
     const {register, handleSubmit, formState: {errors}} = useForm();
-    const {registerUsingEmailAndPassword} = useAuth();
+    const {registerUsingEmailAndPassword, signInWithGoogle} = useAuth();
     const handleRegistration = (data) => {
         registerUsingEmailAndPassword(data.email, data.password)
         .then(result => console.log(result.user))
         .catch(err => console.log(err));
+    }
+
+    const handleGoogleRegistration = () => {
+        signInWithGoogle()
+        .then(result => console.log(result.user))
+        .catch(error => console.log(error));
     }
     return (
         <div className="hero min-h-screen lg:w-[80%] lg:mx-auto">
@@ -37,7 +43,7 @@ const Register = () => {
                         </form>
                         <p className='text-(--color-secondary)'>Already Have an Account? <Link to={'/login'} className='text-(--color-primary)'>Login</Link></p>
                         <p className='text-center'>Or</p>
-                        <button className="btn bg-white text-black">
+                        <button onClick={handleGoogleRegistration} className="btn bg-white text-black">
                             <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
                             Register with Google
                         </button>
